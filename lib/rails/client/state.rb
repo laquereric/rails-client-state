c069591cf9123c20a384b33ec2e2b2704c450f37
Eric Laquer <LaquerEric@gmail.com>
2015-11-23 12:39:54 -0500

    initial from cmd bundle gem rails-client-state

---
+++ lib/rails/client/state.rb
@@ -0,0 +1,9 @@
+require "rails/client/state/version"
+
+module Rails
+  module Client
+    module State
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/client/state/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Client
+    module State
+      VERSION = "0.1.0"
+    end
+  end
+end
